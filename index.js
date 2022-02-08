25c9feb861ddc0acbede833a39edd79b73cc679c
Christopher Magee <chris@csm.cc>
2022-02-08 16:08:47 -0600

    finished the update function

--- index.js
+++ index.js
@@ -9,23 +9,6 @@ const port = 3000
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 
-
-app.get("/", (_, res) => {
-    res.send("Your express app")
-})
-
-app.get('/books', (_, res) => {
-    res.status(200)
-    const bookList = library.books.map( book => book.name)
-    res.json({bookList})
-})
-
-app.get('/books/details', (_, res) => {
-    res.status(200)
-    const bookList = library.books.map( book => book)
-    res.json({bookList})
-})
-
 app.post('/books', (req, res) => {
     const { name, author, description, genres } = req.body
     if (name, author, description, genres) {
@@ -44,6 +27,40 @@ app.post('/books', (req, res) => {
     }
 })
 
+app.get("/", (_, res) => {
+    res.send("Your express app")
+})
+
+app.get('/books', (_, res) => {
+    res.status(200)
+    const bookList = library.books.map( book => book.name)
+    res.json({bookList})
+})
+
+app.get('/books/details', (_, res) => {
+    res.status(200)
+    const bookList = library.books.map( book => book)
+    res.json({bookList})
+})
+
+app.put("/books:name", (req, res) => {
+    const { name, author, description, genres} = req.body
+    if (name, author, description, genres) {
+        if (library.books.filter( book => book.name === name)[0]) {
+            res.status(200)
+            pos = library.books.map( book => book.name).indexOf(name)
+            library.books[pos].name = name
+            library.books[pos].author = author
+            library.books[pos].description = description
+            library.books[pos].genres = genres
+            res.end()
+        }
+    } else {
+        res.status(400)
+        res.json({error: "Book not found"})
+    }
+})
+
 app.delete("/books", (req, res) => {
     const { name } = req.body
     if(name) {
@@ -51,7 +68,6 @@ app.delete("/books", (req, res) => {
             res.status(200)
             library.books.indexOf
             pos = library.books.map( book => book.name).indexOf(name)
-            console.log(pos)
             library.books.splice(pos, 1)
             res.end()
         }
