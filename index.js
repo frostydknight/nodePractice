cce02728de124a88e898f6e574b846b679bdea93
Christopher Magee <chris@csm.cc>
2022-02-08 14:52:27 -0600

    added the get and create functions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const library = require("./data.json")
 
 const app = express()
 const port = 3000
@@ -8,7 +9,37 @@ app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 
 
+app.get("/", (_, res) => {
+    res.send("Your express app")
+})
 
+app.get('/books', (_, res) => {
+    const bookList = library.books.map( book => book.name)
+    res.json({bookList})
+})
+
+app.get('/books/details', (_, res) => {
+    const bookList = library.books.map( book => book)
+    res.json({bookList})
+})
+
+app.post('/books', (req, res) => {
+    const { name, author, description, genres } = req.body
+    if (name, author, description, genres) {
+        const checkBook = library.books.filter( book => book.name === name)[0]
+        if(checkBook){
+            res.status(400)
+            res.json({ error: "Book already exist"})
+        } else {
+            res.status(201)
+            library.books.push ({name, author, description, genres})
+            res.end()
+        }
+    } else {
+        res.status(400)
+        res.json({ error: "missing details"})
+    }
+})
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
 })
